34066cc409e349f7d23361594402a1244c1ca64f
aman.thakur <amanthakur2322@gmail.com>
2024-09-11 15:37:06 +0530

    edit file

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <HashRouter>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </HashRouter>
 );
